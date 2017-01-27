484bf1c191b39bfd04f47889f5e9fb3ded030baa
patrickpirzer <patrick.pirzer@sintec.de>
2017-01-27 13:36:00 +0100

    Add files via upload

---
+++ P16_Test_Validation/P16_Test_Validation/Validation_ValidationRule/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Validation_ValidationRule
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PersonViewModel pvm = new PersonViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = pvm;
+        }
+
+    }
+}
---
+++ P16_Test_Validation/P16_Test_Validation/Validation_ValidationRule/ValidationRules/StringToIntValidationRule.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation_ValidationRule.ValidationRules
+{
+    /// <summary>
+    /// Class for validating int values.
+    /// </summary>
+    public class StringToIntValidationRule : ValidationRule
+    {
+        /// <summary>
+        /// Validates if the given value is a number.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="cultureInfo">The culture info.</param>
+        /// <returns>Returns an object of type ValidationResult.</returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int i;
+            if (int.TryParse(value.ToString(), out i))
+                return new ValidationResult(true, null);
+
+            return new ValidationResult(false, "Please insert a valid number.");
+        }
+
+    }
+}
